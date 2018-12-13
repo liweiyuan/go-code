49fc08b0c1dfa6f936c99db9866b9713a711f377
liweiyuan <liweiyuan@etouch.cn>
2018-12-13 12:59:33 +0800

    enhancement: add template

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"html/template"
+)
+
+type User struct {
+	Username string
+}
+
+type IndexViewModel struct {
+	Title string
+	User  User
+}
+
+//加入模板引擎
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("hello World"))
+		user := User{Username: "wade"}
+		v := IndexViewModel{Title: "HomePage", User: user}
+		tpl, _ := template.New("").ParseFiles("template/index.html")
+		tpl.Execute(writer, &v)
 	})
 	http.ListenAndServe(":8080", nil)
 }
