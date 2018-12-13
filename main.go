29ae828bdc4499ded130ad2f425a6001bae0a8b3
liweiyuan <liweiyuan@etouch.cn>
2018-12-13 13:11:57 +0800

    enhancement: add template

--- main.go
+++ main.go
@@ -9,18 +9,31 @@ type User struct {
 	Username string
 }
 
+type Post struct {
+	User User
+	Body string
+}
+
 type IndexViewModel struct {
 	Title string
 	User  User
+
+	Posts []Post
 }
 
 //加入模板引擎
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		user := User{Username: "wade"}
-		v := IndexViewModel{Title: "HomePage", User: user}
-		tpl, _ := template.New("").ParseFiles("template/index.html")
+		user1 := User{Username: "wade"}
+		user2 := User{Username: "bosh"}
+
+		posts := []Post{
+			Post{User: user1, Body: "Beautiful day in Portland!"},
+			Post{User: user2, Body: "The Avengers movie was so cool!"},
+		}
+		v := IndexViewModel{Title: "HomePage", User: user1, Posts: posts}
+		tpl, _ := template.ParseFiles("template/index.html")
 		tpl.Execute(writer, &v)
 	})
 	http.ListenAndServe(":8080", nil)
