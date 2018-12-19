985b10a1ef73d3c405466d86c92260c40722ea98
liweiyuan <liweiyuan@etouch.cn>
2018-12-19 20:12:30 +0800

    enhancement:sessions

--- controller/g.go
+++ controller/g.go
@@ -1,17 +1,24 @@
 package controller
 
-import "html/template"
+import (
+	"html/template"
+	"github.com/gorilla/sessions"
+)
 
 var (
 	homeController home
 	templates      map[string]*template.Template
+	sessionName    string
+	store          *sessions.CookieStore
 )
 
-func init()  {
-	templates=PopulateTemplate()
+func init() {
+	templates = PopulateTemplate()
+	store = sessions.NewCookieStore([]byte("something-very-secret"))
+	sessionName = "go-code"
 }
 
 //Startup func
-func Startup()  {
+func Startup() {
 	homeController.registerRouters()
 }
